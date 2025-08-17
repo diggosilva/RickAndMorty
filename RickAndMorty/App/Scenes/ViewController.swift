b535927e83fbe9b8eab525fee2e46758c7906a0d
Diggo Silva <rdiggosilva@gmail.com>
2025-08-17 11:39:29 -0300

    Initial configuration

---
+++ RickAndMorty/App/Scenes/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Diggo Silva on 17/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+
+}
