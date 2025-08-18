70efcc3e4747880e58751e56785de9b851806a0d
Diggo Silva <rdiggosilva@gmail.com>
2025-08-18 11:53:10 -0300

    Created object to mirror API

---
+++ RickAndMorty/App/Model/Char.swift
@@ -0,0 +1,47 @@
+struct Char: Codable {
+    let id: Int
+    let name: String
+    let status: String
+    let species: String
+    let type: String
+    let gender: String
+    let origin, location: Location
+    let image: String
+    let episode: [String]
+    let url: String
+    let created: String
+}
+
+struct Location: Codable {
+    let name: String
+    let url: String
+}
+
+struct CharResponse: Codable {
+    let info: Info
+    let results: [Result]
+    
+    struct Info: Codable {
+        let count, pages: Int
+        let next: String
+    }
+
+    struct Result: Codable {
+        let id: Int
+        let name: String
+        let status: String
+        let species: String
+        let type: String
+        let gender: String
+        let origin, location: Location
+        let image: String
+        let episode: [String]
+        let url: String
+        let created: String
+    }
+
+    struct Location: Codable {
+        let name: String
+        let url: String
+    }
+}
---
+++ RickAndMorty/App/Network/Service.swift
@@ -0,0 +1,8 @@
+//
+//  Service.swift
+//  RickAndMorty
+//
+//  Created by Diggo Silva on 17/08/25.
+//
+
+import Foundation
