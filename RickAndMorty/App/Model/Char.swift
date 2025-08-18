58a54fe7aab2eb0932b56c56054ccaca83984515
Diggo Silva <rdiggosilva@gmail.com>
2025-08-18 11:53:42 -0300

    Created custom object

--- RickAndMorty/App/Model/Char.swift
+++ RickAndMorty/App/Model/Char.swift
@@ -1,3 +1,12 @@
+//
+//  Char.swift
+//  RickAndMorty
+//
+//  Created by Diggo Silva on 17/08/25.
+//
+
+import Foundation
+
 struct Char: Codable {
     let id: Int
     let name: String
@@ -15,33 +24,4 @@ struct Char: Codable {
 struct Location: Codable {
     let name: String
     let url: String
-}
-
-struct CharResponse: Codable {
-    let info: Info
-    let results: [Result]
-    
-    struct Info: Codable {
-        let count, pages: Int
-        let next: String
-    }
-
-    struct Result: Codable {
-        let id: Int
-        let name: String
-        let status: String
-        let species: String
-        let type: String
-        let gender: String
-        let origin, location: Location
-        let image: String
-        let episode: [String]
-        let url: String
-        let created: String
-    }
-
-    struct Location: Codable {
-        let name: String
-        let url: String
-    }
 }
