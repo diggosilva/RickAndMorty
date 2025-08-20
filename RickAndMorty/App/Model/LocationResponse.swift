62d1d1e31547cc0ef70868d40b72a43c03a8a5e7
Diggo Silva <rdiggosilva@gmail.com>
2025-08-20 08:51:22 -0300

    Created LocationResponse object to mirro API

--- RickAndMorty/App/Model/LocationResponse.swift
+++ RickAndMorty/App/Model/LocationResponse.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct LocationResponse: Codable {
+    let info: Info
+    let results: [ResultLocation]
+
+    struct Info: Codable {
+        let count: Int
+        let pages: Int
+        let next: String?
+    }
+
+    struct ResultLocation: Codable {
+        let id: Int
+        let name: String
+        let type: String
+        let dimension: String
+        let residents: [String]
+        let url: String
+        let created: String
+    }
+}
