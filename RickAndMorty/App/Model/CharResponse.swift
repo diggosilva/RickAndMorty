12baf5305be85949a125e94204d8ee7954795268
Diggo Silva <rdiggosilva@gmail.com>
2025-08-20 08:07:01 -0300

    Renamed sctructs, Location to LocationDTO and Results to CharDTO

--- RickAndMorty/App/Model/CharResponse.swift
+++ RickAndMorty/App/Model/CharResponse.swift
@@ -9,28 +9,31 @@ import Foundation
 
 struct CharResponse: Codable {
     let info: Info
-    let results: [Result]
-    
+    let results: [CharDTO]
+
     struct Info: Codable {
-        let count, pages: Int
-        let next: String
+        let count: Int
+        let pages: Int
+        let next: String?
+        let prev: String?
     }
 
-    struct Result: Codable {
+    struct CharDTO: Codable {
         let id: Int
         let name: String
         let status: String
         let species: String
         let type: String
         let gender: String
-        let origin, location: Location
+        let origin: LocationDTO
+        let location: LocationDTO
         let image: String
         let episode: [String]
         let url: String
         let created: String
     }
 
-    struct Location: Codable {
+    struct LocationDTO: Codable {
         let name: String
         let url: String
     }
