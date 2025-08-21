b027d747398fd00c96d5ac5eb72bceab887e6b9c
Diggo Silva <rdiggosilva@gmail.com>
2025-08-21 11:51:33 -0300

    Refactored Endpoint file with episode e location

--- RickAndMorty/App/Network/Endpoint.swift
+++ RickAndMorty/App/Network/Endpoint.swift
@@ -28,6 +28,8 @@ enum Endpoint: EndpointProtocol {
     case episodeDetail(id: Int)
     case locationDetail(id: Int)
     case pagedCharacters(page: Int)
+    case pagedEpisodes(page: Int)
+    case pagedLocations(page: Int)
     
     var baseURL: String {
         return "https://rickandmortyapi.com"
@@ -42,6 +44,8 @@ enum Endpoint: EndpointProtocol {
         case .episodeDetail(let id): return "/api/episode/\(id)"
         case .locationDetail(let id): return "/api/location/\(id)"
         case .pagedCharacters: return "/api/character"
+        case .pagedEpisodes: return "/api/episode"
+        case .pagedLocations: return "/api/location"
         }
     }
     
@@ -51,18 +55,21 @@ enum Endpoint: EndpointProtocol {
     
     var queryItems: [URLQueryItem]? {
         switch self {
-        case .pagedCharacters(let page): return [URLQueryItem(name: "page", value: "\(page)")]
+        case .pagedCharacters(let page),
+                .pagedEpisodes(let page), 
+                .pagedLocations(let page):
+            return [URLQueryItem(name: "page", value: "\(page)")]
             
         default: return nil
         }
     }
     
     func createURL() -> URL? {
-        var urlComponets = URLComponents(string: baseURL)
-        urlComponets?.path = path
-        urlComponets?.queryItems = queryItems
+        var urlComponents = URLComponents(string: baseURL)
+        urlComponents?.path = path
+        urlComponents?.queryItems = queryItems
         
-        print("DEBUG: URL.. \(String(describing: urlComponets?.url))")
-        return urlComponets?.url
+        print("DEBUG: URL.. \(String(describing: urlComponents?.url))")
+        return urlComponents?.url
     }
 }
