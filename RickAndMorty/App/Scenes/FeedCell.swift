37382b799d329322d53016bcd35890dc7d142e5d
Diggo Silva <rdiggosilva@gmail.com>
2025-08-24 07:57:58 -0300

    Refactored file e renamed

---
+++ RickAndMorty/App/Scenes/FeedCell.swift
@@ -0,0 +1,8 @@
+//
+//  FeedCell.swift
+//  RickAndMorty
+//
+//  Created by Diggo Silva on 23/08/25.
+//
+
+import Foundation
---
+++ RickAndMorty/App/Scenes/FeedView.swift
@@ -0,0 +1,8 @@
+//
+//  FeedView.swift
+//  RickAndMorty
+//
+//  Created by Diggo Silva on 23/08/25.
+//
+
+import Foundation
