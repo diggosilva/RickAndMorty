b91e7e5af2393277669a200b5347bf199926475d
Diggo Silva <rdiggosilva@gmail.com>
2025-08-24 08:03:20 -0300

    Created FeedView UI

--- RickAndMorty/App/Scenes/FeedView.swift
+++ RickAndMorty/App/Scenes/FeedView.swift
@@ -5,4 +5,59 @@
 //  Created by Diggo Silva on 23/08/25.
 //
 
-import Foundation
+import UIKit
+
+class FeedView: UIView {
+    
+    lazy var collectionView: UICollectionView = {
+        let widthScreen = (UIScreen.main.bounds.width - 60) / 2
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 20
+        layout.itemSize = CGSize(width: widthScreen, height: widthScreen * 1.5)
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.register(FeedCell.self, forCellWithReuseIdentifier: FeedCell.identifier)
+        cv.backgroundColor = .secondarySystemBackground
+        return cv
+    }()
+    
+    lazy var spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.color = .label
+        return spinner
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    private func setupView() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        addSubviews(collectionView, spinner)
+        backgroundColor = .systemBackground
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor),
+        ])
+    }
+}
