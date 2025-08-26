6a01c6582db2ee1c8b0267901d302d20f5498318
Diggo Silva <rdiggosilva@gmail.com>
2025-08-26 09:57:31 -0300

    Implemented applyShadow method

--- RickAndMorty/App/Utils/Extensions.swift
+++ RickAndMorty/App/Utils/Extensions.swift
@@ -11,6 +11,16 @@ extension UIView {
     func addSubviews(_ views: UIView...) {
         views.forEach { addSubview($0) }
     }
+    
+    func applyShadow(view: UIView) {
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOffset = CGSize(width: 5, height: 5)
+        view.layer.shadowOpacity = 0.35
+        view.layer.shadowRadius = 5.0
+        view.layer.cornerRadius = 8
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.systemGray.cgColor
+    }
 }
 
 extension UIViewController {
