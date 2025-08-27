338206d092dec3dc4f838fef62544cbb6b8a52ad
Diggo Silva <rdiggosilva@gmail.com>
2025-08-27 13:05:06 -0300

    Created DetailsViewController and connected witch DetailsViewModel

--- RickAndMorty/App/Scenes/DetailsViewController.swift
+++ RickAndMorty/App/Scenes/DetailsViewController.swift
@@ -5,4 +5,22 @@
 //  Created by Diggo Silva on 27/08/25.
 //
 
-import Foundation
+import UIKit
+
+class DetailsViewController: UIViewController {
+    
+    let viewModel: DetailsViewModel
+    
+    init(viewModel: DetailsViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+    
+}
