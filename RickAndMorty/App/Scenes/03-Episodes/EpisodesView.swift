a601cd48283cf39830ba4acc9ad91a9705e862f5
Diggo Silva <rdiggosilva@gmail.com>
2025-09-02 14:47:20 -0300

    Refactored, removed unused code

---
+++ RickAndMorty/App/Scenes/03-Episodes/EpisodesView.swift
@@ -0,0 +1,8 @@
+//
+//  EpisodesView.swift
+//  RickAndMorty
+//
+//  Created by Diggo Silva on 02/09/25.
+//
+
+import Foundation
---
+++ RickAndMorty/App/Scenes/03-Episodes/EpisodesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LocationViewController.swift
+//  RickAndMorty
+//
+//  Created by Diggo Silva on 02/09/25.
+//
+
+import UIKit
+
+class EpisodesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Location"
+        view.backgroundColor = .systemYellow
+    }
+    
+}
