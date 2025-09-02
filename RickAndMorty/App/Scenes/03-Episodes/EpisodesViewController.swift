bc78ae466bd32251acdd76433b22cc282a691e26
Diggo Silva <rdiggosilva@gmail.com>
2025-09-02 14:49:52 -0300

    Connected ViewController with View

--- RickAndMorty/App/Scenes/03-Episodes/EpisodesViewController.swift
+++ RickAndMorty/App/Scenes/03-Episodes/EpisodesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  LocationViewController.swift
+//  EpisodesViewController.swift
 //  RickAndMorty
 //
 //  Created by Diggo Silva on 02/09/25.
@@ -9,10 +9,43 @@ import UIKit
 
 class EpisodesViewController: UIViewController {
     
+    private let episodesView = EpisodesView()
+    
+    override func loadView() {
+        super.loadView()
+        view = episodesView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.title = "Location"
-        view.backgroundColor = .systemYellow
+        setupNavigationBar()
+        setupDelegatesAndDataSources()
+    }
+    
+    private func setupNavigationBar() {
+        navigationItem.title = "Episodes"
+    }
+    
+    private func setupDelegatesAndDataSources() {
+        episodesView.tableView.dataSource = self
+    }
+}
+
+extension EpisodesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = "Episode \(indexPath.row + 1)"
+        content.secondaryText = "Season 1"
+        content.image = UIImage(systemName: "chevron.right")
+        cell.contentConfiguration = content
+        
+        return cell
     }
     
 }
