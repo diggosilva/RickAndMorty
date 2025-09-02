85dcea7323ebb524cbb3a1a66b5db0c781c41fc4
Diggo Silva <rdiggosilva@gmail.com>
2025-09-02 14:48:16 -0300

    Created tableView for episodes

--- RickAndMorty/App/Scenes/03-Episodes/EpisodesView.swift
+++ RickAndMorty/App/Scenes/03-Episodes/EpisodesView.swift
@@ -5,4 +5,40 @@
 //  Created by Diggo Silva on 02/09/25.
 //
 
-import Foundation
+import UIKit
+
+class EpisodesView: UIView {
+    
+    lazy var tableView: UITableView = {
+        let tv = UITableView()
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    private func setupView() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        addSubviews(tableView)
+        backgroundColor = .systemBackground
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}
