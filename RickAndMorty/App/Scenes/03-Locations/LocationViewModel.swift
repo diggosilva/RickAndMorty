87e79451e9f08e96b04b49872efdc009f4ed21df
Diggo Silva <rdiggosilva@gmail.com>
2025-09-05 11:39:29 -0300

    Created LocationViewModel

--- RickAndMorty/App/Scenes/03-Locations/LocationViewModel.swift
+++ RickAndMorty/App/Scenes/03-Locations/LocationViewModel.swift
@@ -6,3 +6,63 @@
 //
 
 import Foundation
+import Combine
+
+enum LocationViewControllerStates {
+    case loading
+    case loaded
+    case error
+}
+
+protocol LocationViewModelProtocol: StatefulViewModel where State == LocationViewControllerStates {
+    func numberOfRows() -> Int
+    func location(at indexPath: IndexPath) -> Location
+    func fetchLocations()
+}
+
+class LocationViewModel {
+    
+    private var locations: [Location] = []
+    private var page: Int = 1
+    private var isLoading: Bool = false
+    private var hasMorePages: Bool = true
+    
+    @Published private var state: LocationViewControllerStates = .loading
+    
+    var statePublisher: AnyPublisher<LocationViewControllerStates, Never> {
+        $state.eraseToAnyPublisher()
+    }
+    
+    private let service: ServiceProtocol
+    
+    init(service: ServiceProtocol = Service()) {
+        self.service = service
+    }
+    
+    func numberOfRows() -> Int {
+        return locations.count
+    }
+    
+    func location(at indexPath: IndexPath) -> Location {
+        return locations[indexPath.row]
+    }
+    
+    func fetchLocations() {
+        guard !isLoading, hasMorePages else { return }
+        
+        isLoading = true
+        
+        Task { @MainActor in
+            do {
+                let newLocations = try await service.getLocations(page: page)
+                locations.append(contentsOf: newLocations.locations)
+                state = .loaded
+                page += 1
+                hasMorePages = newLocations.hasMorePages
+            } catch {
+                state = .error
+            }
+            isLoading = false
+        }
+    }
+}
