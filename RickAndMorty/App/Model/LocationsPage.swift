8a1d6dddaa93817b6d70d383a4b221f3ed73276f
Diggo Silva <rdiggosilva@gmail.com>
2025-09-05 11:36:52 -0300

    Created LocationsPage

---
+++ RickAndMorty/App/Model/LocationsPage.swift
@@ -0,0 +1,13 @@
+//
+//  LocationsPage.swift
+//  RickAndMorty
+//
+//  Created by Diggo Silva on 05/09/25.
+//
+
+import Foundation
+
+struct LocationsPage {
+    let locations: [Location]
+    let hasMorePages: Bool
+}
---
+++ RickAndMorty/App/Scenes/03-Locations/LocationViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LocationViewModel.swift
+//  RickAndMorty
+//
+//  Created by Diggo Silva on 04/09/25.
+//
+
+import Foundation
