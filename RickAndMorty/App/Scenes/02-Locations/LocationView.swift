9f374d174c2d7636546eeb3b5874bf82d8a389eb
Diggo Silva <rdiggosilva@gmail.com>
2025-09-07 12:41:31 -0300

    Created LocationView

---
+++ RickAndMorty/App/Scenes/02-Locations/LocationView.swift
@@ -0,0 +1,44 @@
+//
+//  LocationView.swift
+//  RickAndMorty
+//
+//  Created by Diggo Silva on 06/09/25.
+//
+
+import UIKit
+
+class LocationView: UIView {
+    
+    lazy var tableView: UITableView = {
+        let tv = UITableView()
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    private func setupView() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        addSubviews(tableView)
+        backgroundColor = .systemBackground
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
