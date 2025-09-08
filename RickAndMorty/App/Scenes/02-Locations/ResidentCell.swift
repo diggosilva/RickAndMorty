1975267945db5769ed3fe004afe6c8e94f19017f
Diggo Silva <rdiggosilva@gmail.com>
2025-09-08 11:39:59 -0300

    Created ResidentCell custom

--- RickAndMorty/App/Scenes/02-Locations/ResidentCell.swift
+++ RickAndMorty/App/Scenes/02-Locations/ResidentCell.swift
@@ -5,4 +5,56 @@
 //  Created by Diggo Silva on 07/09/25.
 //
 
-import Foundation
+import UIKit
+
+class ResidentCell: UICollectionViewCell {
+    
+    static let identifier = "ResidentCell"
+    
+    lazy var avatarImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.layer.cornerRadius = 8
+        iv.image = UIImage(systemName: "person.circle")
+        return iv
+    }()
+    
+    lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .preferredFont(forTextStyle: .headline)
+        label.text = "John Doe"
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    private func setupView() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        addSubviews(avatarImageView, nameLabel)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+            avatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 76),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor),
+            
+            nameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: avatarImageView.leadingAnchor),
+            nameLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8),
+        ])
+    }
+}
