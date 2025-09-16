28c4eef10f4c2bc9e8e62276b45207daecc86c6a
Diggo Silva <rdiggosilva@gmail.com>
2025-09-16 10:39:54 -0300

    Refactored fetchLocations method

--- RickAndMorty/App/Scenes/02-Locations/LocationViewModel.swift
+++ RickAndMorty/App/Scenes/02-Locations/LocationViewModel.swift
@@ -16,13 +16,13 @@ enum LocationViewControllerStates {
 
 protocol LocationViewModelProtocol: StatefulViewModel where State == LocationViewControllerStates {
     func numberOfRows() -> Int
-    func location(at indexPath: IndexPath) -> Location
+    func location(at indexPath: IndexPath) -> LocationChar
     func fetchLocations()
 }
 
 class LocationViewModel {
     
-    private var locations: [Location] = []
+    private var locationsChar: [LocationChar] = []
     private var page: Int = 1
     private var isLoading: Bool = false
     private var hasMorePages: Bool = true
@@ -41,11 +41,11 @@ class LocationViewModel {
     }
     
     func numberOfRows() -> Int {
-        return locations.count
+        return locationsChar.count
     }
     
-    func location(at indexPath: IndexPath) -> Location {
-        return locations[indexPath.row]
+    func location(at indexPath: IndexPath) -> LocationChar {
+        return locationsChar[indexPath.row]
     }
     
     func fetchLocations() {
@@ -56,11 +56,23 @@ class LocationViewModel {
         Task { @MainActor in
             do {
                 let newLocations = try await service.getLocations(page: page)
-                locations.append(contentsOf: newLocations.locations)
+                var ids: [Int] = []
                 
                 for location in newLocations.locations {
-                    let characterIDs = extractCharacterIDs(from: location.residents)
-                    print("IDs DOS PERSONAGENS: \(characterIDs)")
+                    ids.append(contentsOf: location.residentsIDs())
+                }
+                
+                let residents = try await service.getMultipleCharacters(ids: ids)
+                
+                for location in newLocations.locations {
+                    var chars: [Char] = []
+                    
+                    for resident in residents {
+                        if location.residentsIDs().contains(resident.id) {
+                            chars.append(resident)
+                        }
+                    }
+                    locationsChar.append(LocationChar(location: location, chars: chars))
                 }
                 state = .loaded
                 page += 1
@@ -70,9 +82,5 @@ class LocationViewModel {
             }
             isLoading = false
         }
-    }
-    
-    private func extractCharacterIDs(from urls: [String]) -> [Int] {
-        return urls.compactMap { Int($0.split(separator: "/").last ?? "") }
     }
 }
