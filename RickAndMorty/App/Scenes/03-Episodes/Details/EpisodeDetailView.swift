e9da490e4348919935e67c6d3177cc07e78f8576
Diggo Silva <rdiggosilva@gmail.com>
2025-09-21 12:50:42 -0300

    Created EpisodeDetailView file

---
+++ RickAndMorty/App/Scenes/03-Episodes/Details/EpisodeDetailView.swift
@@ -0,0 +1,53 @@
+//
+//  EpisodeDetailView.swift
+//  RickAndMorty
+//
+//  Created by Diggo Silva on 20/09/25.
+//
+
+import UIKit
+
+final class EpisodeDetailView: UIView {
+    
+    lazy var tableView: UITableView = {
+        let tv = UITableView()
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tv
+    }()
+    
+    lazy var spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        return spinner
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    private func setupView() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        addSubviews(tableView, spinner)
+        backgroundColor = .systemBackground
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+}
