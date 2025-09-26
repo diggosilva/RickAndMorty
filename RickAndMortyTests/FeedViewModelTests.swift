4105f3eaacfa7fd86f5217803a8cb7e7f81fe195
Diggo Silva <rdiggosilva@gmail.com>
2025-09-26 13:38:16 -0300

    Xcode configuration

---
+++ RickAndMortyTests/FeedViewModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  RickAndMortyTests.swift
+//  RickAndMortyTests
+//
+//  Created by Diggo Silva on 26/09/25.
+//
+
+import Testing
+
+struct RickAndMortyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
